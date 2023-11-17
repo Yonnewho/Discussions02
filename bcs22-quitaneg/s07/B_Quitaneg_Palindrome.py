3d6e79b02067fc62d57db7ad52b11cfe567548fb
Yonnewho <142853009+Yonnewho@users.noreply.github.com>
2023-11-17 17:12:24 +0800

    Update B_Quitaneg_Palindrome.py

--- bcs22-quitaneg/s07/B_Quitaneg_Palindrome.py
+++ bcs22-quitaneg/s07/B_Quitaneg_Palindrome.py
@@ -21,10 +21,15 @@ class Stack:
         temp.next = None
         return temp.data
 
-def palindrome_checker(sentence):
-    def clean_sentence(s):
-        return "".join(char.upper() for char in s if char.isalnum())
+def clean_sentence(s):
+    cleaned_sentence = ""
+    for char in s:
+        # Check for alphanumeric characters
+        if ('a' <= char <= 'z') or ('A' <= char <= 'Z') or ('0' <= char <= '9'):
+            cleaned_sentence += char.lower()
+    return cleaned_sentence
 
+def is_palindrome(sentence):
     stack = Stack()
     cleaned_sentence = clean_sentence(sentence)
 
@@ -37,14 +42,19 @@ def palindrome_checker(sentence):
 
     return cleaned_sentence, reversed_sentence
 
-# Get the sentence from user input
-user_sentence = input("Enter a sentence to check for palindrome: ")
-cleaned_sentence, reversed_sentence = palindrome_checker(user_sentence)
+while True:
+    # Get the sentence from user input
+    user_sentence = input("Enter a sentence to check for palindrome (type 'E' to quit): ")
+    
+    if user_sentence.lower() == 'e':
+        break
+    
+    cleaned_sentence, reversed_sentence = is_palindrome(user_sentence)
 
-if cleaned_sentence == reversed_sentence:
-    print(f'The sentence "{user_sentence}" is a palindrome. \n ----------------------------------------------------------------')
-else:
-    print(f'The sentence "{user_sentence}" is not a palindrome. \n ---------------------------------------------------------------')
+    if cleaned_sentence == reversed_sentence:
+        print(f'The sentence "{user_sentence}" is a palindrome.')
+    else:
+        print(f'The sentence "{user_sentence}" is not a palindrome.')
 
-print(f'For comparison \n "Cleaned Sentence: {cleaned_sentence}" \n "Reversed Sentence: {reversed_sentence}"')
-
+    print("Cleaned Sentence:", cleaned_sentence)
+    print("Reversed Sentence:", reversed_sentence)
